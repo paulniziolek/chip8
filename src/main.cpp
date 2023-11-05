1c44330c5e9743f26183900afcf8c02ccec50f4e
paulniziolekimprint <paul.niziolek@imprint.co>
2023-11-05 00:57:20 -0400

    Some testing with SDL window

--- src/main.cpp
+++ src/main.cpp
@@ -5,11 +5,29 @@
 int main(int argc, char *argv[]) {
     Chip8 user_chip8;
     argv[1] = "roms/ultimatetictactoe.ch8"; // temp, just for testing
-    argv[2] = "log"; // temp, just for testing
+    //argv[2] = "log"; // temp, just for testing
 
     if (argv[2] != NULL && strcmp(argv[2], "log")==0) {
         spdlog::set_level(spdlog::level::debug);
     }
+
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        spdlog::error("SDL fails to initialize: {}", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Window* window = nullptr;
+    SDL_Renderer* renderer = nullptr;
+    if (SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL, &window, &renderer) < 0) {
+        spdlog::error("SDL failed to create window & renderer: {}", SDL_GetError());
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_UpdateWindowSurface(window);
+    SDL_Delay(10000);
+    
 
     const char* rompath = argv[1];
     if (rompath == NULL) {
@@ -30,8 +48,11 @@ int main(int argc, char *argv[]) {
         while (user_chip8.is_paused_flag) {
             // process_user_input
         }
+
         break;
     }
+    
+    SDL_DestroyWindow(window);
+    SDL_Quit();
     std::cout << "Finished running";
-
 }
