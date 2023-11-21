7ff111810ba3730d4ded4f00c18be8d1e433921b
paulniziolekimprint <paul.niziolek@imprint.co>
2023-11-20 19:17:03 -0500

    Fixed SDL window not popping up bug

--- src/main.cpp
+++ src/main.cpp
@@ -11,24 +11,6 @@ int main(int argc, char *argv[]) {
         spdlog::set_level(spdlog::level::debug);
     }
 
-    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
-        spdlog::error("SDL fails to initialize: {}", SDL_GetError());
-        exit(EXIT_FAILURE);
-    }
-
-    SDL_Window* window = nullptr;
-    SDL_Renderer* renderer = nullptr;
-    if (SDL_CreateWindowAndRenderer(SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL, &window, &renderer) < 0) {
-        spdlog::error("SDL failed to create window & renderer: {}", SDL_GetError());
-    }
-
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-    SDL_RenderPresent(renderer);
-    SDL_UpdateWindowSurface(window);
-    SDL_Delay(10000);
-    
-
     const char* rompath = argv[1];
     if (rompath == NULL) {
         spdlog::error("ROM Path not specified.");
@@ -38,20 +20,57 @@ int main(int argc, char *argv[]) {
 
     load_rom(&user_chip8, rompath);
 
+    if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
+        spdlog::error("SDL fails to initialize: {}", SDL_GetError());
+        exit(EXIT_FAILURE);
+    }
+    
+    // TODO: Implement resolution scaling in window & renderer
+    // TODO: Change resolution scaling based on config file
+    SDL_Window* window = SDL_CreateWindow("Chip8 Emulator", 
+                                        SDL_WINDOWPOS_UNDEFINED, 
+                                        SDL_WINDOWPOS_UNDEFINED, 
+                                        SCREEN_WIDTH, 
+                                        SCREEN_HEIGHT, 
+                                        SDL_WINDOW_SHOWN | SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI);
+
+    if (window == nullptr) {
+        spdlog::error("SDL failed to create window: {}", SDL_GetError());
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    if (renderer == nullptr) {
+        spdlog::error("SDL failed to create renderer: {}", SDL_GetError());
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        exit(EXIT_FAILURE);
+    }
+
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
+    SDL_UpdateWindowSurface(window);
+
     // TODO: CPU clockcycle debugging here
     
     while(user_chip8.is_running_flag) {
 
         execute_instruction(&user_chip8);
 
+        SDL_Event event;
+        SDL_PollEvent(&event);
+
         // TODO: process_user_input()
         while (user_chip8.is_paused_flag) {
             // process_user_input
         }
 
-        break;
+        
     }
     
+    SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     std::cout << "Finished running";
