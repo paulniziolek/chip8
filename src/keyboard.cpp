8aa52ba8b0612ae9febfa3bd1f79cdd553776db9
paulniziolekimprint <paul.niziolek@imprint.co>
2023-11-21 00:24:05 -0500

    Added disabling debug logs with keyboard

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -9,13 +9,19 @@ void process_user_input(Chip8* sys) {
             spdlog::info("{} key pressed, switching pause flag to {}", event.key.keysym.sym, sys->is_paused_flag^1);
             sys->is_paused_flag ^= 1;
             break;
+        case SDLK_F9:
+            if (event.key.type == SDL_KEYUP) continue;
+            spdlog::set_level(spdlog::level::info);
+            break;
         case SDLK_F10:
             if (event.key.type == SDL_KEYUP) continue;
             spdlog::info("{} key pressed, restarting system", event.key.keysym.sym);
             break;
         case SDLK_F12:
+        if (event.key.type == SDL_KEYUP) continue;
             spdlog::info("{} key pressed, shutting off system", event.key.keysym.sym);
             sys->is_running_flag = false;
+            sys->is_paused_flag = false;
             break;
         }
     }
