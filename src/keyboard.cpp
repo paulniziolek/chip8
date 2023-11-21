1e503e7d524b2cf37a3aa50da57c6ccec887bd92
paulniziolekimprint <paul.niziolek@imprint.co>
2023-11-21 01:26:01 -0500

    Allowing exit on close

--- src/keyboard.cpp
+++ src/keyboard.cpp
@@ -3,6 +3,13 @@
 void process_user_input(Chip8* sys) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT) {
+            // the same as F12
+            spdlog::info("program closed, shutting off system");
+            sys->is_running_flag = false;
+            sys->is_paused_flag = false;
+        }
+
         switch (event.key.keysym.sym) {
         case SDLK_SPACE:
             if (event.key.type == SDL_KEYUP) continue;
@@ -11,14 +18,14 @@ void process_user_input(Chip8* sys) {
             break;
         case SDLK_F9:
             if (event.key.type == SDL_KEYUP) continue;
-            spdlog::set_level(spdlog::level::info);
+            spdlog::get_level() == spdlog::level::debug ? spdlog::set_level(spdlog::level::info) : spdlog::set_level(spdlog::level::debug);
             break;
         case SDLK_F10:
             if (event.key.type == SDL_KEYUP) continue;
             spdlog::info("{} key pressed, restarting system", event.key.keysym.sym);
             break;
         case SDLK_F12:
-        if (event.key.type == SDL_KEYUP) continue;
+            if (event.key.type == SDL_KEYUP) continue;
             spdlog::info("{} key pressed, shutting off system", event.key.keysym.sym);
             sys->is_running_flag = false;
             sys->is_paused_flag = false;
